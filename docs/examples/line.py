2055ac010ca7bbc90e79dca69f84a3cb670da1e7
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2025-03-11 16:15:46 +0000

    wip: completing chain output

--- docs/examples/line.py
+++ docs/examples/line.py
@@ -5,6 +5,7 @@ import tqdm
 
 
 jax.config.update('jax_enable_x64', True) 
+jax.config.update('jax_platform_name', 'cpu')
 
 rng_key = jax.random.PRNGKey(0)
 n_dims = 3
@@ -93,7 +94,6 @@ logL_birth = np.concatenate((dead.logL_birth, live.logL_birth), dtype=float)
 pid = np.concatenate((dead.pid, live.pid), dtype=int)
 start_pid = np.concatenate((dead.mcmc_chain_start_pid, -1 * jnp.ones(n_live, dtype=int)), dtype=int)
 end_pid = np.concatenate((dead.mcmc_chain_end_pid, -1 * jnp.ones(n_live, dtype=int)), dtype=int)
-dead.mcmc_chain
 data = np.concatenate([
     np.column_stack([v for v in dead.particles.values()]),
     np.column_stack([v for v in live.particles.values()])
@@ -108,15 +108,96 @@ samples.gui()
 samples.to_csv('line.csv') 
 
 
-# Also have access to the mcmc chain positions
+
+# BEGINN:
+# starting point are the current compression factors t_i
+print('compression factors')
+t = np.log(samples.nlive/(samples.nlive+1))
+print(t)
+# or the logX
+logX = samples.logX()
+# first compression factor is estimated right now from beta distribution:
+log_t1 = logX.iloc[1] - logX.iloc[0]
+print('log_t1', log_t1)
+
+# estimating t1 using mcmc samples with the loglikelihoods of the mcmc samples -- this is likely the one you need
+print('dead mcmc chain loglikelihood shape', dead.mcmc_chain.loglikelihood.shape)
+# take the dead points from the first iteration:
+print('dead after first it')
+# Q: is there a way to do this without losing named tuple access?
+dead_after_first_it = dead[:n_delete]
+logLs = dead_after_first_it[1]
+mcmc_chain = dead_after_first_it[5]
+mcmc_chain_start_pid = dead_after_first_it[6]
+mcmc_chain_end_pid = dead_after_first_it[7]
+
+print('mcmc_chain')
+print(mcmc_chain)
+
+chain_idx = 0
+print('mcmc positions for chain', chain_idx)
+print(len(mcmc_chain.position['c'][chain_idx]))
+print(mcmc_chain.position['c'][chain_idx][0])
+print(mcmc_chain.position['m'][chain_idx][0])
+print(mcmc_chain.position['sigma'][chain_idx][0])
+first_chain_position = {
+    'c': mcmc_chain.position['c'][chain_idx][0],
+    'm': mcmc_chain.position['m'][chain_idx][0],
+    'sigma': mcmc_chain.position['sigma'][chain_idx][0],
+    }
+last_chain_position = {
+    'c': mcmc_chain.position['c'][chain_idx][-1],
+    'm': mcmc_chain.position['m'][chain_idx][-1],
+    'sigma': mcmc_chain.position['sigma'][chain_idx][-1],
+}
+
+print(first_chain_position)
+print(last_chain_position)
+print('start and end pids for chain ', chain_idx)
+print(mcmc_chain_start_pid[chain_idx])
+print(mcmc_chain_end_pid[chain_idx])
+print(f'real chain {chain_idx} start loc', samples[samples['pid']==mcmc_chain_start_pid[chain_idx]][['c', 'm', 'sigma']].values)
+print('chain start', first_chain_position)
+print(f'real chain {chain_idx} end loc', samples[samples['pid']==mcmc_chain_end_pid[chain_idx]][['c', 'm', 'sigma']].values)
+print('chain last', last_chain_position)
+# A: the real chain start is missing from the mcmc chain (which is what we expect from this implementation of ns)
+
+
+# log likelihoods
+# print(logL_birth)
+# print(logL)
+
+
+exit(0)
+
 dead.mcmc_chain.position['m']
 dead.mcmc_chain.position['c']
 dead.mcmc_chain.position['sigma']
 
-# And their loglikelihoods -- this is likely the one you need
-dead.mcmc_chain.loglikelihood.shape
+
+# Q1: why does the next thing not work?
+# print('live mcmc chain loglikelihood shape', live.mcmc_chain.loglikelihood.shape)
 
 # If you like, you can tag these onto the array
 chain_loglikelihoods = np.concatenate([dead.mcmc_chain.loglikelihood, np.nan * np.ones((n_live, num_mcmc_steps))], axis=0)
-chain_loglikelihoods
+# print('chain loglikelihoods shape', chain_loglikelihoods.shape)
 samples[[f'logL_{i}' for i in range(num_mcmc_steps)]] = chain_loglikelihoods
+
+# The log of the prior volume contained within each iso - likelihood contour.
+print('samples.logX')
+samples.logX()
+print(samples.logL.shape)
+
+
+# now we use the MCMC chains to re-estimate the first (probably best?) compression factor
+
+# use a single  corrected compression factor to correct all volumes
+
+# re-estimate logZ
+
+
+
+
+
+
+# Q4: what is meant by eq 4?
